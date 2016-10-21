bd5681841baf646f628e1da92f17a494f17591fc
yongsokheng <yongsokheng@ymail.com>
2016-10-21 15:57:53 +0700

    Follow user list

---
+++ app/controllers/follow_infos_controller.rb
@@ -0,0 +1,19 @@
+class FollowInfosController < ApplicationController
+  load_and_authorize_resource :user
+
+  def followings
+    @followings = @user.followeds.preload(followed: :user_profile).page params[:page]
+    respond_to do |format|
+      format.html {render "profiles/index"}
+      format.js
+    end
+  end
+
+  def followers
+    @followers = @user.followers.preload(follower: :user_profile).page params[:page]
+    respond_to do |format|
+      format.html {render "profiles/index"}
+      format.js
+    end
+  end
+end
