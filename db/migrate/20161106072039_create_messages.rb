fd56ef7f5e6d8ccb767f98f2e93121058871f00c
yongsokheng <yongsokheng@ymail.com>
2016-12-04 23:55:24 +0700

    chat attachment

--- db/migrate/20161106072039_create_messages.rb
+++ db/migrate/20161106072039_create_messages.rb
@@ -2,6 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.string :content
+      t.string :file_attachment
+      t.string :image_attachment
       t.references :conversation, foreign_key: true, index: true
       t.integer :sender_id, index: true
       t.integer :receiver_id, index: true
